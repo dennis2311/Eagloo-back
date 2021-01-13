ee068f287dd255e5d95bdfbaf3ea79379b1659c2
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2021-01-13 21:09:37 +0900

    CORS 세팅

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 app.use(cors());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://eagloo.netlifyapp.com");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
